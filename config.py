455ab565bb5c2522ad6ee480b4ef05bfff1fb794
Brad <bradegbert26@gmail.com>
2020-11-26 14:00:31 -0600

    Uploaded main files

---
+++ config.py
@@ -0,0 +1,44 @@
+# Instagram Bot Config
+
+details = {
+    "first_name": "Dwight",
+    "last_name": "Schrute",
+    "email": "duhwhiteshroot@gmail.com",
+    "password": "theoffice123"
+}
+
+
+comments = [
+    "Love it!",
+    "Dinosaurs...",
+    "Lmaooo",
+    "Awesome",
+    "Gross...",
+    "Wtf",
+    "Meh",
+    "What a snack!",
+    "This makes my butthole tingle!",
+    "I\'m glad you\'re just as weird as I am.",
+    "When someone smiles at me, all I see is a chimpanzee begging for its life.",
+    "Why are all these people here? There\'s too many people on this earth. We need a new plague.",
+    "I wish I could menstruate",
+    "Growing up I performed my own circumcision.",
+    "I need coffee before I can comprehend the profundity of this post",
+    "That wasn\'t Giuliani\'s hair dye running, that was his reptilian skin suit melting",
+    "This makes me want to take a granola bath!",
+    "God... I miss Tony Stark, he\'d know what to do right now",
+    "I\'ve used so much hand sanitizer that my skin is cracked like a dry river bed!",
+    "This is so amazing that it makes me want to start painting watercolor stick figures!",
+    "Have you ever noticed that a basketball sort of feels like an enlarged testicle?",
+    "Nice shot bruh, if I could shoot like that I\'d have at least 22 kids by now!",
+    "You should do one with baby Yoda. He\'s the most adorable goblin man I\'ve ever seen! I should know I\'ve got goblins living in the coal mine behind my house!",
+    "So spicy, like a dragon fart 🔥",
+    "I find dragons extremely exciting. They hold so much power yet their touch can be like that of a velvety soft cat\'s tongue!",
+    "I ordered tape worm eggs off Amazon to help me lose weight and now I\'m hungry all the time 😭",
+    "My flatulence has been smelling like rotten eggs as of late...",
+    "This post is obviously haunted. Does no one see the demon face hidden in the background?",
+    "Congrats! You\'ve been tongued by Dwight. I\'ve been exercising my tongue muscle by liking and commenting on Instagram posts 👅",
+    "I wish I was this talented. All I can do is articulate my individual beard hairs like a gentle breeze flowing through a field of wheat.",
+    "I received a past life regression from a magic woman where I lived as a pheasant hunter in the 1620\'s and you resemble my spouse perfectly!",
+    "This makes me want to rub my sphincter!"
+]
