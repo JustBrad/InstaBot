420eab99fdd18aa3ee4d248fea65879412a88a70
Brad Egbert <bradegbert26@gmail.com>
2020-11-30 20:34:28 -0600

    Fixed comment percentage

--- bot_rp.py
+++ bot_rp.py
@@ -39,7 +39,7 @@ class InstaBot:
         # Probabilities (1 in [x] chance)
         self.prob_like = 4      # 25%
         self.prob_follow = 25   # 4%
-        self.prob_comment = 1  # 10%
+        self.prob_comment = 10  # 10%
 
         # Create path variable for driver
         self.PATH = "C:\Program Files (x86)\chromedriver.exe"
